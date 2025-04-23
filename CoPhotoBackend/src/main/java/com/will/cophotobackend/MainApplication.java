00059dd621185789aa313ad368a42d17ec8145ab
byecheems <127092401+byecheems@users.noreply.github.com>
2025-04-23 09:23:14 +0800

    commit

---
+++ CoPhotoBackend/src/main/java/com/will/cophotobackend/MainApplication.java
@@ -0,0 +1,13 @@
+package com.will.cophotobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoPhotoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoPhotoBackendApplication.class, args);
+	}
+
+}
---
+++ CoPhotoBackend/src/test/java/com/will/cophotobackend/CoPhotoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.will.cophotobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoPhotoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
